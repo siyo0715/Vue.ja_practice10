1a13a8146c4b4f167bcd3c0ede403848926bdf16
向山　翔 <hms0175km@gmail.com>
2021-12-21 22:29:54 +0900

    [Add]watch

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ new Vue({
             return this.counter > 3 ? '3より上' : '3より下'
         }
     },
-    // methods: {
-    //     lessThanThreeMethod: function () {
-    //         return this.counter > 3 ? '3より上' : '3より下'
-    //     }
-    // },
+    watch: {
+        // 約3秒くらいで数値が0になる
+        counter: function () {
+            const vm = this;
+            setTimeout(function () {
+                vm.counter = 0
+            },3000)
+        }
+    }
 })
